34ac0f95a668c3ecd9ce7805b3f31b3d37789381
wildarte <wildarte10@gmail.com>
2023-01-27 20:22:56 -0300

    atualizacao do menu header e comecei a fazer o footer

--- javascript/theme-script.js
+++ javascript/theme-script.js
@@ -1,6 +1,7 @@
 const open_search = document.querySelector('.btn_search.open_search');
 const search_over = document.querySelector('.search_over');
 const btn_slose_search = document.getElementById('btn_slose_search');
+const hero_top = document.querySelector('.hero_top');
 
 open_search.addEventListener('click', () => {
 
@@ -11,5 +12,23 @@ open_search.addEventListener('click', () => {
 btn_slose_search.addEventListener('click', () => {
 
     search_over.classList.remove('open_search');
+
+});
+
+
+//========================== scroll menu header =============================
+document.addEventListener('scroll', (e) => {
+
+    let hw = window.scrollY;
+
+    let height_header = hero_top.getBoundingClientRect().height;
+
+    if(hw > 200){
+        hero_top.classList.add('over_hero_top');
+        document.body.style.marginTop = height_header+"px";
+    }else{
+        hero_top.classList.remove('over_hero_top');
+        document.body.style.marginTop = "0px";
+    }
 
 });
