f113f95259243424043e874248f328615c034694
wildarte <wildarte10@gmail.com>
2023-01-28 18:23:37 -0300

    criacao da parte de acesso rápido

--- javascript/theme-script.js
+++ javascript/theme-script.js
@@ -2,6 +2,7 @@ const open_search = document.querySelector('.btn_search.open_search');
 const search_over = document.querySelector('.search_over');
 const btn_slose_search = document.getElementById('btn_slose_search');
 const hero_top = document.querySelector('.hero_top');
+const float_btn_up = document.querySelector('.float_btn_up');
 
 open_search.addEventListener('click', () => {
 
@@ -26,9 +27,17 @@ document.addEventListener('scroll', (e) => {
     if(hw > 200){
         hero_top.classList.add('over_hero_top');
         document.body.style.marginTop = height_header+"px";
+        float_btn_up.classList.add('show_btn_up');
     }else{
         hero_top.classList.remove('over_hero_top');
         document.body.style.marginTop = "0px";
+        float_btn_up.classList.remove('show_btn_up');
     }
+
+});
+
+float_btn_up.addEventListener('click', () => {
+
+    window.scrollTo(0,0)
 
 });
