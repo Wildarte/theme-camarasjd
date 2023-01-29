dad9cceb94b730c332723ede682cfa7f03c90d14
wildarte <wildarte10@gmail.com>
2023-01-29 18:41:54 -0300

    comecei a fazer o slide hero

--- javascript/theme-script.js
+++ javascript/theme-script.js
@@ -3,6 +3,7 @@ const search_over = document.querySelector('.search_over');
 const btn_slose_search = document.getElementById('btn_slose_search');
 const hero_top = document.querySelector('.hero_top');
 const float_btn_up = document.querySelector('.float_btn_up');
+const hero_slide_uni = document.querySelectorAll('.hero_slide_uni');
 
 open_search.addEventListener('click', () => {
 
@@ -41,3 +42,26 @@ float_btn_up.addEventListener('click', () => {
     window.scrollTo(0,0)
 
 });
+
+if(hero_slide_uni){
+
+
+    let uni = 0;
+
+    setInterval(function(){
+        hero_slide_uni.forEach((item) => {
+            item.classList.remove('hero_slide_uni_active');    
+        })
+        hero_slide_uni[uni].classList.add('hero_slide_uni_active');
+        uni += 1;
+        if(uni == hero_slide_uni.length){
+            uni = 0
+        }
+    }, 5000);
+
+
+
+
+}
+
+console.log(hero_slide_uni.length)
